0b5f6a08dceac928eeb959bc05d610db79ae0840
Belorina <ozcan_berrin@hotmail.com>
2021-05-20 15:50:00 +0200

    ennemy tombe avec random sur axe X (loop a faire)

--- pyhton_berrin.py
+++ pyhton_berrin.py
@@ -1,31 +1,58 @@
 # Pyhton file by Berrin 
 
 # Ennemie position aleatoire en haut
-
-from random import randint
+from random import randint, random
 
 WIDTH = 800
 HEIGHT = 600
 
-all_ennemie = []
+all_ennemy = []
+ennemy_speed = [0, 3]
 
-# Placer les Ennemies
-def setup_ennemie():
-    global all_ennemie
-    
-    for x in range (0, 800, 100):
-        for y in range(0, 30, 30):
-            ennemie = Actor("brick-perso", anchor=["left", "top"]) 
-            ennemie.pos = [x, y]
-            all_ennemie.append(ennemie)
-setup_ennemie()
+game_over = 0
+
+# Placer les Ennemies en haut en dehors de la fenetre 
+def setup_ennemy():
+    global all_ennemy
+    global game_over
+
+    while game_over < 4 :
+
+        game_over = game_over + 1
+        random_number = randint(100,200)
+
+        for x in range (0,800, random_number):           # debut, fin, espacement
+            print(randint(1, 800))
+            for y in range(0, 50, random_number):          # changez parraport au assest ennemie (grandeur largeurs etc? ?? 
+
+
+                ennemy = Actor("brick-perso", anchor=["left", "top"]) 
+                ennemy.pos = [x, y]
+                all_ennemy.append(ennemy)
+            
+setup_ennemy()
 
 def draw():
-    global all_ennemie
+    global all_ennemy
 
     screen.clear()
     # bg1.draw()
     # player.draw()
 
-    for ennemie in all_ennemie:
-        ennemie.draw()
+    for ennemy in all_ennemy:
+        ennemy.draw()
+
+def update(dt):
+
+     # move down  
+    for ennemy in all_ennemy:
+        new_x = ennemy.pos[0] + ennemy_speed[0]
+        new_y = ennemy.pos[1] + ennemy_speed[1]
+
+        ennemy.pos = [new_x, new_y]
+
+
+    # # check boundries
+    # if ball.right >= WIDTH or ball.left <= 0:  # ball.right, donne la 
+    #                                         # position a droit de la ball
+    #     invert_horizontal_speed()
