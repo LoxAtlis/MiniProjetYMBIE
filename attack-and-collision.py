d2352d7e75cfdff542f2d76bc5954cfddb09d7cf
Yasmine El Messaoudi <yasnardam@hotmail.com>
2021-05-20 15:37:44 +0200

    added gameover but not finished

--- attack-and-collision.py
+++ attack-and-collision.py
@@ -3,9 +3,7 @@ HEIGHT = 600
 
 LEFT = 1
 RIGHT = 3
-
-# bg1 = Actor("bg1")
-# bg1.pos = [400,300]
+loose = False
 
 player = Actor("player")
 player.pos = [400,550]
@@ -14,12 +12,16 @@ bullet = Actor("projectile")
 bullet.pos = player.pos
 bullet_speed = [0, -4]
 
+gameover = Actor("go-im")
+
 def draw():
     screen.clear()
 
-    # bg1.draw()
-    player.draw()
     bullet.draw()
+    player.draw()
+
+    if loose:
+        gameover.draw()
 
 def on_mouse_move(pos):
     player.pos = [pos[0],player.pos[1]]
@@ -27,16 +29,19 @@ def on_mouse_move(pos):
 #click droit ou gauche
 def on_mouse_down(button):
     if button == LEFT or button == RIGHT:
-        print("HELLO")
+        shoot()
 
-#fonction pour tirer une bullete
-#def shoot():
-	#bullet = Actor("projectile")
-	#bullet.pos = player.pos
+#fonction pour tirer une bullet
+def shoot():
+	bullet = Actor("projectile")
+	bullet.pos = player.pos
 
 def update():
     global bullet
     global bullet_speed
-    new_x = bullet.pos[0] + bullet_speed[0] # new_x = position de la bullete + vitesse de la bullete (3)
-    new_y = bullet.pos[1] + bullet_speed[1] # new_y = position de la bullete + vitesse de la bullete (-3)
+    new_x = bullet.pos[0] + bullet_speed[0]
+    new_y = bullet.pos[1] + bullet_speed[1]
     bullet.pos = [new_x, new_y]
+
+    # if (ennemy.bottom > HEIGHT or ennemy.colliderect(player)) and not loose:
+	#     loose = True
