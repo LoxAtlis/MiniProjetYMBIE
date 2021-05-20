2bedcdc2d9ba2d220d32f93f25e17b16e6344836
Elisabetta Saporetti <elisabetta.saporetti@yahoo.it>
2021-05-20 16:28:45 +0200

    "reboot asteroid"

--- miao_proj.py
+++ miao_proj.py
@@ -13,8 +13,6 @@ all_asteroid = []
 
 
 # asteroid_apparition = 50
-
-
 random_number = randint(400,600)
 for x in range (0,800, random_number):           
     for y in range(0, 50, random_number):          
@@ -23,18 +21,43 @@ for x in range (0,800, random_number):
         asteroid_speed = [0,300]
         all_asteroid.append(asteroid)
 
-for x in range (200,600, random_number):           
-    for y in range(0, 50, random_number):          
-        asteroid_petit = Actor("asteroid64", anchor=["left", "top"])
-        asteroid_petit.pos = [x,y]
-        asteroid_petit_speed = [0,200]
-        all_asteroid.append(asteroid_petit)
+    for x in range (200,600, random_number):           
+        for y in range(0, 50, random_number):          
+            asteroid_petit = Actor("asteroid64", anchor=["left", "top"])
+            asteroid_petit.pos = [x,y]
+            asteroid_petit_speed = [0,200]
+            all_asteroid.append(asteroid_petit)
+
+def reboot():
+    global  all_asteroid
+    global asteroid
+    global asteroid_speed
+    global asteroid_petit
+    global asteroid_petit_speed
+    random_number = randint(400,600)
+    for x in range (0,800, random_number):           
+        for y in range(0, 50, random_number):          
+            asteroid = Actor("asteroid128", anchor=["left", "top"])
+            asteroid.pos = [x,y]
+            asteroid_speed = [0,300]
+            all_asteroid.append(asteroid)
+
+    for x in range (200,600, random_number):           
+        for y in range(0, 50, random_number):          
+            asteroid_petit = Actor("asteroid64", anchor=["left", "top"])
+            asteroid_petit.pos = [x,y]
+            asteroid_petit_speed = [0,200]
+            all_asteroid.append(asteroid_petit)
+
 
 
     
 def update_asteroid(dt):
     global  all_asteroid
     global asteroid
+    global asteroid_speed
+    global asteroid_petit
+    global asteroid_petit_speed
 
 
     for asteroid in all_asteroid:   # (pour creer un asteroid)
@@ -48,10 +71,13 @@ def update_asteroid(dt):
         new_x = asteroid_petit.pos[0] + asteroid_petit_speed[0] *dt
         asteroid_petit.pos = [new_x,new_y]
 
+    if asteroid.pos[1] > 600:
+        reboot()
     
-    
+
 def update(dt):
     update_asteroid(dt)
+    
    
 def draw():
     screen.clear()
